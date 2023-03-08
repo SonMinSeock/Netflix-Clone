8abf5ae8d419b7611235cd8a0ed2e45eda1e8dcb
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-08 18:17:54 +0900

    router basename 속성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Tv from "./Routes/Tv";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Headers />
       <Switch>
         <Route path={["/tv", "/tv/:movieId"]}>
